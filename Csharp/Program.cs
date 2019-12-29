be0b3dad4d429d06d0c6e70ba0f2c099185c7ffa
tc021 <sypnik21@gmail.com>
2019-12-29 20:22:05 +0100

    init

---
+++ Csharp/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pesel");
+            string p = Console.ReadLine();
+            
+            Console.ReadKey();
+            /*Console.WriteLine("Hello World!");
+            Console.ReadKey();*/
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
